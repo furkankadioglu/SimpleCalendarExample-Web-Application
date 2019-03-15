a74e23cf51942acbc044912b0b786474fe232bda
Furkan Kadıoğlu <furkan.kadioglu@gmail.com>
2019-03-15 23:06:20 +0300

    Added new Restful API routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +12,24 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::any('api', 'Api\ApiController@status');
+Route::any('status', 'Api\ApiController@status');
+
+
+
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+    Route::post('login', 'Api\AuthController@login');
+    Route::post('register', 'Api\AuthController@register');
+    Route::post('logout', 'Api\AuthController@logout');
+    Route::post('refresh', 'Api\AuthController@refresh');
+    Route::post('me', 'Api\AuthController@me');
+});
+
+
+Route::group(['middleware' => 'api', 'prefix' => 'calendarEvents'], function ($router) {
+    Route::get('getEvents', 'Api\CalendarEventController@getEvents');
+    Route::post('create', 'Api\CalendarEventController@create');
+    Route::post('update/{calendar_event_id}', 'Api\CalendarEventController@update');
+    Route::post('delete/{calendar_event_id}', 'Api\CalendarEventController@delete');
 });
