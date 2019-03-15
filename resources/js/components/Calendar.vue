7da0bcf1a9ba098b10395bd2a30d90851fe70088
Furkan Kadıoğlu <furkan.kadioglu@gmail.com>
2019-03-15 23:12:01 +0300

    Complete frontend init

---
+++ resources/js/components/Calendar.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="container">
+        <h1 class="text-center">Hello {{ currentUser.name }}</h1>
+        <br/>
+
+
+        <div id="calendar"></div>
+    </div>
+</template>
+
+<script>
+import fullcalendar from 'fullcalendar';
+import $ from 'jquery';
+import axios from 'axios';
+
+ export default {
+    mounted() {
+        axios.defaults.headers.common['Authorization'] =  'Bearer ' + this.$store.getters.currentUser.token
+
+        $('#calendar').fullCalendar({
+            eventSources: [
+                {
+                    url: '/api/calendarEvents/getEvents', 
+                    headers: {
+                            'Authorization': 'Bearer ' + this.$store.getters.currentUser.token
+                    },
+                    success: function(response) { 
+                        return response.payload; 
+                    }
+                }
+            ],
+            selectable: true,
+            selectHelper: true,
+            select: function(start, end) {
+                var title = prompt('Event Title:');
+                var eventData;
+                if (title) {
+                    eventData = {
+                        title: title,
+                        start: start,
+                        end: end
+                    };
+                    axios.post('/api/calendarEvents/create', {
+                        name: title,
+                        date: start
+                    });
+                    $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+                }
+                $('#calendar').fullCalendar('unselect');
+            },
+            eventRender: function(event, element) {
+                element.append( "<span class='closeon' style='font-size: 18, text-center: 'center' '>X</span>" );
+                element.find(".closeon").click(function() {
+                    axios.post('/api/calendarEvents/delete/' + event.id);
+                    $('#calendar').fullCalendar('removeEvents',event._id);
+                });
+            },
+            eventClick: function(event) {
+                if (event.title) {
+                    var title = prompt('Event Title:', event.title);
+                    event.title = title;
+
+                    axios.post('/api/calendarEvents/update/' + event.id, {
+                        name: event.title,
+                        date: event.start
+                    });
+
+                    $('#calendar').fullCalendar('updateEvent', event)
+
+                }
+            }
+        })
+    },
+    computed:{
+        currentUser(){
+            return this.$store.getters.currentUser
+        }
+    } 
+ }
+ </script>
+
+ <style>
+ .closeon {
+    text-align: center;
+    flex: 1;
+    color: darkred;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    font-weight: bold;
+    display: block;
+ }
+ </style>
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="container">
+    <h1>Please log in and go to Calendar page</h1>
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
