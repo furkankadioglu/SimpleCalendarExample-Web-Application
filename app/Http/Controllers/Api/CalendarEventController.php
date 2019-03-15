951200a56e60d464eeaa7a6978e2cb012b26e8d7
Furkan Kadıoğlu <furkan.kadioglu@gmail.com>
2019-03-15 23:03:02 +0300

    Create, update, delete methods added

--- app/Http/Controllers/Api/CalendarEventController.php
+++ app/Http/Controllers/Api/CalendarEventController.php
@@ -12,7 +12,6 @@ use App\Http\Resources\CalendarEventResource;
 
 class CalendarEventController extends Controller
 {
-
     /**
      * Create a new AuthController instance.
      *
@@ -35,6 +34,105 @@ class CalendarEventController extends Controller
         return [
             "success" => true,
             "payload" => new CalendarEventsCollection(auth()->user()->calendarEvents)
+        ];
+    }
+
+    /**
+     * Calendar Event create method
+     *
+     * @param Request $request
+     * 
+     * @return  \Illuminate\Http\JsonResponse
+     */
+    public function create(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:6|max:90',
+            'date' => 'required|date_format:Y-m-d',
+        ]);
+
+        if ($validator->fails()) {
+            return [
+                'success' => false,
+                'errors' => $validator->errors()
+            ];
+        }
+
+        $calendarEvent = new CalendarEvent;
+        $calendarEvent->name = request()->name;
+        $calendarEvent->date = request()->date;
+        $calendarEvent->user_id = auth()->user()->id;
+        $calendarEvent->save();
+
+        // Time consumer TODO: Destroy the main getEvents cache
+
+        return [
+            "success" => true,
+            "payload" => new CalendarEventResource($calendarEvent)
+        ];
+    }
+
+    /**
+     * Calendar Event update method
+     *
+     * @param Request $request
+     * @param Integer $calendar_event_id
+     * 
+     * @return  \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, $calendar_event_id)
+    {
+        $calendarEvent = CalendarEvent::userEvents()->find($calendar_event_id);
+
+        if(!$calendarEvent)
+        {
+            return [
+                'success' => false,
+                'errors' => [
+                    "noEvent" => "Please check the event id."
+                ]
+            ];
+        }
+
+        $calendarEvent->name = request()->name;
+        $calendarEvent->date = request()->date;
+        $calendarEvent->user_id = auth()->user()->id;
+        $calendarEvent->save();
+
+        // Time consumer TODO: Destroy the main getEvents cache
+
+        return [
+            "success" => true,
+            "payload" => new CalendarEventResource($calendarEvent)
+        ];
+    }
+
+    /**
+     * Calendar Event delete method
+     *
+     * @param Request $request
+     * @param Integer $calendar_event_id
+     * 
+     * @return  \Illuminate\Http\JsonResponse
+     */
+    public function delete(Request $request, $calendar_event_id)
+    {
+        $calendarEvent = CalendarEvent::userEvents()->find($calendar_event_id);
+    
+        if(!$calendarEvent)
+        {
+            return [
+                'success' => false,
+                'errors' => [
+                    "noEvent" => "Please check the event id."
+                ]
+            ];
+        }
+
+        $calendarEvent->delete();
+
+        return [
+            "success" => true
         ];
     }
 }
