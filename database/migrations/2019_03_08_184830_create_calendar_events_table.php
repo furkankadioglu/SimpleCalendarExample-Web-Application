9e5bd2727906ca9bda64825b17bcaa6e4f4d6385
Furkan Kadıoğlu <furkan.kadioglu@gmail.com>
2019-03-15 23:16:22 +0300

    Added calendar_events migration

---
+++ database/migrations/2019_03_08_184830_create_calendar_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCalendarEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calendar_events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 90)->default('Unnamed Event');
+            $table->date('date');
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('calendar_events', function($table) {
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calendar_events');
+    }
+}
