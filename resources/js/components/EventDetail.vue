64fa2d7a448a498f163ab73233898746ed543891
Furkan Kadıoğlu <furkan.kadioglu@gmail.com>
2019-04-28 00:55:16 +0300

    empty EventDetail implemented

---
+++ resources/js/components/EventDetail.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="eventDetail" class="eventDetail">
+        XXXX
+    </div>
+</template>
+
+<script>
+import $ from 'jquery';
+import axios from 'axios';
+import moment from 'moment';
+
+ export default {
+    mounted() {
+        axios.defaults.headers.common['Authorization'] =  'Bearer ' + this.$store.getters.currentUser.token        
+    },
+    data: function() {
+        return {
+            event: null
+        }
+    }
+ }
+ </script>
+
+ <style>
+#eventDetail
+{
+    float: left;
+}
+ </style>
--- resources/js/components/MainApp.vue
+++ resources/js/components/MainApp.vue
@@ -1,16 +1,25 @@
 <template>
     <div class="main">
        <Header />
-       <div class="content">
+       <div class="content" id="mainContent">
            <router-view></router-view>
        </div>
+       <EventDetail />
     </div>
 </template>
 
 <script>
 import Header from './Header.vue';
+import EventDetail from './EventDetail.vue';
 export default {
     name: 'main-App',
-    components: { Header }
+    components: { Header, EventDetail }
 }
 </script>
+
+<style>
+#mainContent
+{
+    float: left;
+}
+</style>
