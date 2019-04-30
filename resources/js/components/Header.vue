b08fc42bfc459f562be8111684832fbeef35ffbd
Furkan Kadıoğlu <furkan.kadioglu@gmail.com>
2019-04-30 13:14:54 +0300

    Mini header init

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,47 +1,54 @@
 <template>
-    <nav class="navbar navbar-expand-md navbar-light navbar-laravel mb-4">
-      <div class="container">
-            <template v-if="!currentUser">
-                <router-link class="navbar-brand" to="/">Calendar Example</router-link>
-            </template>
-            <template v-else>
-                <router-link class="navbar-brand" to="/dashboard">Calendar Example</router-link>
-            </template>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+    <div id="header">
+    <nav class="navbar navbar-expand-md navbar-light navbar-top">
+        <div class="container">
+                <template v-if="!currentUser">
+                    <router-link class="navbar-brand" to="/">Calendar Example</router-link>
+                </template>
+                <template v-else>
+                    <router-link class="navbar-brand" to="/dashboard">Calendar Example</router-link>
+                </template>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <div class="navbar-nav ml-auto">
-                    <template v-if="!currentUser">
-                        <li>
-                            <router-link to="/login" class="nav-link">Login</router-link>
-                        </li>
-                        <li>
-                            <router-link to="/register" class="nav-link">Register</router-link>
-                        </li>
-                    </template>
-                    <template v-else>
-                        <li>
-                            <router-link to="/dashboard" class="nav-link">Dashboard</router-link>
-                        </li>
-                        <li>
-                            <router-link to="/calendar" class="nav-link">Calendar</router-link>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                {{currentUser.name}} <span class="caret"></span>
-                            </a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="navbar-nav ml-auto">
+                        <template v-if="!currentUser">
+                            <li>
+                                <router-link to="/login" class="nav-link">Login</router-link>
+                            </li>
+                            <li>
+                                <router-link to="/register" class="nav-link">Register</router-link>
+                            </li>
+                        </template>
+                        <template v-else>
+                            <li>
+                                <router-link to="/dashboard" class="nav-link">Dashboard</router-link>
+                            </li>
+                            <li>
+                                <router-link to="/calendar" class="nav-link">Calendar</router-link>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    {{currentUser.name}} <span class="caret"></span>
+                                </a>
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a href="#!" @click.prevent="logout" class="dropdown-item">Logout</a>
-                            </div>
-                        </li>
-                    </template>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a href="#!" @click.prevent="logout" class="dropdown-item">Logout</a>
+                                </div>
+                            </li>
+                        </template>
+                    </div>
                 </div>
             </div>
+        </nav>
+        <div class="navbar navbar-expand-md navbar-light navbar-sub mb-4">
+            <div class="container">
+                asdasdsa
+            </div>
         </div>
-    </nav>
+    </div>
 </template>
 
 <script>
